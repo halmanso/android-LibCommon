48e42d9f8f75a0de7aeb9611d8e70353663d61c4
Andrew Balmos <abalmos@purdue.edu>
2012-08-08 02:11:22 -0400

    Initial commit.
    Added rock API to Library.
    Classes which handle rocks content provider and the Rocks.java wrapper
    classes was moved here. Resource (such as images/drawable) also exist
    here.
    
    Added SlideLayout to library for use in other apps

---
+++ src/edu/purdue/libwaterapps/db/RockDB.java
@@ -0,0 +1,84 @@
+package edu.purdue.libwaterapps.db;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import edu.purdue.libwaterapps.provider.RockProvider;
+
+/*
+ * Manages the SQLite database for the RockProvider
+ * 
+ */
+public class RockDB extends SQLiteOpenHelper {
+	// A tool help keep dates formated correctly in the database
+	private static SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static final String db_name = "rocks";
+	private static final int version = 1;
+	
+	/* DB uses a singleton pattern */
+	public RockDB(Context context) {
+		super(context, db_name, null, version);
+		
+		// We always store and use UTC time
+		dateFormater.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
+	
+	/*
+	 * Called by Android when no SQLite database exists. This is 
+	 * where the database should be created.
+	 */
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE " + RockProvider.Constants.TABLE + "(" +
+				RockProvider.Constants._ID + " INTEGER PRIMARY KEY," +
+				RockProvider.Constants.TRELLO_ID + " TEXT," +
+				RockProvider.Constants.LAT + " INTEGER," +
+				RockProvider.Constants.LON + " INTEGER," +
+				RockProvider.Constants.PICKED + " TEXT," +
+				RockProvider.Constants.COMMENTS + " TEXT," +
+				RockProvider.Constants.PICTURE + " TEXT," +
+				RockProvider.Constants.UPDATE_TIME + " TEXT," +
+				RockProvider.Constants.TRELLO_PULL_TIME + " TEXT)");
+	}
+	
+	/*
+	 * Called by Android when a SQLite database exists but the current version
+	 * is different.
+	 * 
+	 * Our action is the just delete the old DB and make the new (cloud should
+	 * restore the data)
+	 */
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.d("DB", "Updating RockApp Database");
+		db.execSQL("DROP TABLE IF EXISTS " + RockProvider.Constants.TABLE);
+		this.onCreate(db);
+	}
+	
+	/*
+	 * Takes in a date and returns it in a string format
+	 */
+	public static String dateFormat(Date date) {
+		return RockDB.dateFormater.format(date);
+	}
+	
+	/*
+	 * Takes in a string formated by dateFormat() and returns the
+	 * original date.
+	 */
+	public static Date dateParse(String date) {
+		Date d;
+		
+		try {
+			d = RockDB.dateFormater.parse(date);
+		} catch (ParseException e) {
+			d = new Date(0);
+		}
+		
+		return d;
+	}
+}
---
+++ src/edu/purdue/libwaterapps/rock/Rock.java
@@ -0,0 +1,257 @@
+package edu.purdue.libwaterapps.rock;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.google.android.maps.GeoPoint;
+
+import edu.purdue.libwaterapps.db.RockDB;
+import edu.purdue.libwaterapps.provider.RockProvider;
+
+/* A class which knows everything about a given rock */
+public class Rock {
+	private int id = -1;
+	private String trelloId;
+	private int lat;
+	private int lon;
+	private boolean picked;
+	private String comments;
+	private String picture;
+	private Date updateDate; 
+	private Date trelloPullDate;
+	private Context context;
+	public static String[] rockProjection = {
+		RockProvider.Constants._ID,
+		RockProvider.Constants.TRELLO_ID,
+		RockProvider.Constants.LAT,
+		RockProvider.Constants.LON,
+		RockProvider.Constants.PICKED,
+		RockProvider.Constants.COMMENTS,
+		RockProvider.Constants.PICTURE,
+		RockProvider.Constants.UPDATE_TIME,
+		RockProvider.Constants.TRELLO_PULL_TIME
+	};
+	
+	public Rock(Context context) {
+		this.updateDate = new Date(0);
+		this.updateDate = new Date(0);
+		this.context = context;
+	}
+
+	public Rock(Context context, GeoPoint point, boolean picked) {
+		lat = point.getLatitudeE6();
+		lon = point.getLongitudeE6();
+		this.picked = picked;
+		this.context = context;
+	}
+	
+	public static Rock getRock(Context context, int id) {
+		Rock rock = null;
+		String where = RockProvider.Constants._ID + "=?";
+		String[] whereArgs = { Integer.toString(id) };
+		
+		Cursor cursor = context.getContentResolver().query(RockProvider.Constants.CONTENT_URI,
+														   Rock.rockProjection, where,
+														   whereArgs, "");
+		
+		if(cursor != null && cursor.getCount() == 1) {
+			cursor.moveToFirst();
+			rock = Rock.translateCursorToRock(context, cursor);
+		}
+		
+		cursor.close();
+				
+		return rock;
+	}
+	
+	public static ArrayList<Rock> getAllRocks(Context context) { 
+		ArrayList<Rock> rocks = new ArrayList<Rock>();
+		
+		Cursor cursor = context.getContentResolver().query(RockProvider.Constants.CONTENT_URI, 
+														   Rock.rockProjection, null, null, "");
+		
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()) {
+			rocks.add(Rock.translateCursorToRock(context, cursor));
+			
+			cursor.moveToNext();
+		}
+		
+		cursor.close();
+		
+		return rocks;
+	}
+	
+	public static ArrayList<Rock> getAllPickedRocks(Context context) { 
+		ArrayList<Rock> rocks = new ArrayList<Rock>();
+		String where = "picked = ?";
+		String[] whereArgs = { "true" };
+		
+		Cursor cursor = context.getContentResolver().query(RockProvider.Constants.CONTENT_URI, 
+														   Rock.rockProjection, where, whereArgs, "");
+		
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()) {
+			rocks.add(Rock.translateCursorToRock(context, cursor));
+			cursor.moveToNext();
+		}
+		
+		cursor.close();
+		
+		return rocks;
+	}
+	
+	public static ArrayList<Rock> getAllNonPickedRocks(Context context) { 
+		ArrayList<Rock> rocks = new ArrayList<Rock>();
+		String where = "picked = ?";
+		String[] whereArgs = { "false" };
+		
+		Cursor cursor = context.getContentResolver().query(RockProvider.Constants.CONTENT_URI, 
+														   Rock.rockProjection, where, whereArgs, "");
+		
+		cursor.moveToFirst();
+		while(!cursor.isAfterLast()) {
+			rocks.add(Rock.translateCursorToRock(context, cursor));
+			cursor.moveToNext();
+		}
+		
+		cursor.close();
+		
+		return rocks;
+	}
+	
+	
+	
+	/* Create a rock in the database */
+	public void save() {
+		/* Set the update time to the current time */
+		this.setUpdateDate(new Date());
+		
+		ContentValues vals = new ContentValues();
+		vals.put(RockProvider.Constants.TRELLO_ID, "");
+		vals.put(RockProvider.Constants.LAT, this.getLat());
+		vals.put(RockProvider.Constants.LON, this.getLon());
+		vals.put(RockProvider.Constants.PICKED, Boolean.toString(this.isPicked()));
+		vals.put(RockProvider.Constants.COMMENTS, this.getComments());
+		vals.put(RockProvider.Constants.PICTURE, this.getPicture());
+		vals.put(RockProvider.Constants.UPDATE_TIME, RockDB.dateFormat(this.getUpdateDate()));
+		vals.put(RockProvider.Constants.TRELLO_PULL_TIME, "");
+		
+		if(this.id < 0) {
+			Uri uri = this.context.getContentResolver().insert(
+					RockProvider.Constants.CONTENT_URI,
+					vals);
+			
+			this.setId((int)ContentUris.parseId(uri));
+		} else {
+			String where = RockProvider.Constants._ID + "=?";
+			String[] whereArgs = {Integer.toString(this.id)};
+			
+			this.context.getContentResolver().update(
+					RockProvider.Constants.CONTENT_URI,
+					vals, where, whereArgs);
+		}
+	}
+	
+	private static Rock translateCursorToRock(Context context, Cursor cursor) {
+		Rock rock = new Rock(context);
+		
+		rock.setId(Integer.parseInt(cursor.getString(0)));
+		rock.setTrelloId(cursor.getString(1));
+		rock.setLat(cursor.getInt(2));
+		rock.setLon(cursor.getInt(3));
+		rock.setPicked(Boolean.parseBoolean(cursor.getString(4)));
+		rock.setComments(cursor.getString(5));
+		rock.setPicture(cursor.getString(6));
+		rock.setUpdateDate(RockDB.dateParse(cursor.getString(7)));
+		rock.setTrelloPullDate(RockDB.dateParse(cursor.getString(8)));
+		
+		return rock;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTrelloId() {
+		return this.trelloId;
+	}
+
+	public void setTrelloId(String trelloId) {
+		this.trelloId = trelloId;
+	}
+
+	public int getLat() {
+		return this.lat;
+	}
+
+	public void setLat(int lat) {
+		this.lat = lat;
+	}
+
+	public int getLon() {
+		return this.lon;
+	}
+
+	public void setLon(int lon) {
+		this.lon = lon;
+	}
+
+	public boolean isPicked() {
+		return this.picked;
+	}
+
+	public void setPicked(boolean picked) {
+		this.picked = picked;
+	}
+
+	public String getComments() {
+		return this.comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public String getPicture() {
+		return this.picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public Date getUpdateDate() {
+		return this.updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Date getTrelloPullDate() {
+		return this.trelloPullDate;
+	}
+
+	public void setTrelloPullDate(Date trelloPullDate) {
+		this.trelloPullDate = trelloPullDate;
+	}
+	
+	public Context getContext() {
+		return this.context;
+	}
+	
+	public void setContext(Context context) {
+		this.context = context;
+	}
+}
